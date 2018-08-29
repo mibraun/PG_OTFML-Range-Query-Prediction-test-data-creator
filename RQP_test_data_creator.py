2073b1be4df65abf7024e06da7313474f1c7da89
Mirko Juergens <mirkoj@mail.upb.de>
2018-08-29 13:02:39 +0200

    Fixes order

--- RQP_test_data_creator.py
+++ RQP_test_data_creator.py
@@ -20,6 +20,175 @@ from random import randint
 k = 8
 
 
+def create_test_data(file_path, sampling_method="uniform", num_test_data_points=None):
+    raw_data = pd.DataFrame(arff.loadarff(file_path)[0])
+    X = raw_data.iloc[:, :-1].get_values()
+    Y = raw_data.iloc[:, -1:]
+    N = raw_data.shape[0]
+    if num_test_data_points is None:
+        num_test_data_points = int(np.floor(0.3 * N))
+
+    return create_test_data_internal(N=N, X=X, Y=Y, dataset_name=file_path, sampling_method=sampling_method,
+                                     num_test_data_points=num_test_data_points, numeric_features=range(len(X[0])),
+                                     categoric_features=[])
+
+
+
+# Creates the test-data & training data based on the given
+def create_test_data_internal(X, Y, N, numeric_features, dataset_name, categoric_features=[], num_test_data_points=None,
+                              sampling_method="random_points", noises=[0.1, 0.01, 0.001]):
+    if num_test_data_points is None:
+        num_test_data_points = N
+
+    X_gt_train, Y_gt_train = standardize_data(categoric_features=categoric_features, X=X, Y=Y,
+                                             numeric_features=numeric_features)
+
+    synth_ground_truth = train_neural_network(X_gt_train=X_gt_train, Y_gt_train=Y_gt_train)
+
+    # Original X data with synthetic ground truth Y-values as training data for RQP
+    X_RQP_train = X_gt_train
+    Y_RQP_train = np.reshape(synth_ground_truth.predict(X_RQP_train), (N, 1))
+
+    column_names_train = {}
+    num_features = X_RQP_train.shape[1]
+
+    for feature in range(num_features):
+        column_names_train[feature] = 'x' + str(feature)
+
+    train_data = []
+    for noise in noises:
+        y_copy = np.copy(Y_RQP_train)
+        for i in range(len(y_copy)):
+            y_copy[i] = y_copy[i] + np.random.normal(0, noise)
+        data_train = np.concatenate([X_RQP_train, y_copy], axis=1)
+        data_train_df = pd.DataFrame(data_train)
+        data_train_df.rename(columns=column_names_train, inplace=True)
+        train_data.append(data_train_df)
+
+
+    data_test_df = create_rqp_data(X_RQP_train=X_RQP_train,
+                                                  numeric_features=numeric_features,
+                                                  categoric_features=categoric_features,
+                                                  synth_ground_truth=synth_ground_truth,
+                                                  num_test_data_points=num_test_data_points,
+                                                  sampling_method=sampling_method)
+
+    write_to_file(data_train_dfs=train_data, data_test_df=data_test_df, dataset_name=dataset_name, noises=noises,
+                      sampling_method=sampling_method)
+
+
+def standardize_data(categoric_features, X, Y, numeric_features):
+    print("Standardizing...")
+    standardized_y = StandardScaler().fit_transform(Y)
+
+    # We don't want to standardize the categorical data
+    if len(categoric_features) is 0:
+        X_norm = StandardScaler().fit_transform(X)
+    else:
+        X_norm = CustomScaler(bin_vars_index=categoric_features, cont_vars_index=numeric_features).fit_transform(X)
+    return X_norm, np.ravel(standardized_y)
+
+
+def train_neural_network(X_gt_train, Y_gt_train):
+    print("Optimizing DNN...")
+
+    # MLP regressor with alpha-parameter and hidden layer sizes determined by grid search cross validation
+    param_grid = {'classify__alpha': 10.0 ** -np.arange(1, 7),
+                  'classify__activation': ['logistic'],
+                  'classify__hidden_layer_sizes': [(layer1, layer2)
+                                                   for layer1 in range(10, 28, 4)
+                                                   for layer2 in range(10, 28, 4)]}
+
+    synth_ground_truth_pipe = Pipeline([('classify', MLPRegressor())])
+    synth_ground_truth = GridSearchCV(synth_ground_truth_pipe, param_grid, n_jobs=1)
+    synth_ground_truth.fit(X_gt_train, Y_gt_train)
+    print("CV score:", synth_ground_truth.best_score_)
+    print("Finished optimizing, params are:")
+    print(synth_ground_truth.best_params_)
+    return synth_ground_truth
+
+
+def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_data_points, categoric_features,
+                     sampling_method):
+    # Generate interval-valued test data points for RQP
+    # by optimizing on random intervals on synthetic ground truth
+    data_test = []
+    num_features = X_RQP_train.shape[1]
+
+    numeric_min = []
+    numeric_max = []
+    for feature in numeric_features:
+        feature_column = X_RQP_train[:, feature]
+        numeric_min.append(np.min(feature_column))
+        numeric_max.append(np.max(feature_column))
+
+    for i in range(num_test_data_points):
+        NUM_RANDOM_RESTARTS = 10
+        if sampling_method == "length_uniform":
+            feature_intervals = interval_sample_length(num_features, categoric_features)
+        elif sampling_method == "uniform":
+            feature_intervals = interval_sample_uniform(numeric_features=numeric_features,
+                                                        categoric_features=categoric_features)
+        elif sampling_method == "random_points":
+            feature_intervals = interval_sample_random_points(numeric_features=numeric_features,
+                                                              categoric_features=categoric_features, X=X_RQP_train)
+        elif sampling_method == "length_exponential":
+            feature_intervals = interval_sample_length_exponential(numeric_min=numeric_min, numeric_max=numeric_max,
+                                                                   categoric_features=categoric_features)
+        elif sampling_method == "random_features":
+            feature_intervals = interval_sample_random_features(X_RQP_train)
+
+        # Restarts for blackbox optimization: middle of intervals plus 10 randomly chosen restarts
+        initial_guesses = []
+        mid_guess = []
+        for feature_interval in feature_intervals:
+            mid_guess.append(np.mean([feature_interval[0], feature_interval[1]]))
+        initial_guesses.append(mid_guess)
+        for j in range(NUM_RANDOM_RESTARTS):
+            rand_guess = []
+            for feature_interval in feature_intervals:
+                rand_feature_value = (feature_interval[1] - feature_interval[0]) \
+                                     * np.random.random_sample() + feature_interval[0]
+                rand_guess.append(rand_feature_value)
+            initial_guesses.append(rand_guess)
+        y_mins = []
+        y_maxs = []
+        for initial_guess in initial_guesses:
+            y_min_result = optimize.minimize(lambda x: synth_ground_truth.predict(x.reshape(1, -1)),
+                                             x0=initial_guess, bounds=feature_intervals)
+            y_max_result = optimize.minimize(lambda x: (-1) * synth_ground_truth.predict(x.reshape(1, -1)),
+                                             x0=initial_guess, bounds=feature_intervals)
+            y_mins.append(synth_ground_truth.predict(y_min_result.x.reshape(1, -1))[0])
+            y_maxs.append(synth_ground_truth.predict(y_max_result.x.reshape(1, -1))[0])
+        y_min = np.min(y_mins)
+        y_max = np.max(y_maxs)
+        feature_intervals.append((y_min, y_max))
+        data_test.append(feature_intervals)
+
+    # unpack intervals for test data
+    data_test_unpacked = []
+    for data in data_test:
+        feature_intervals_unpacked = []
+        for feature in data:
+            feature_intervals_unpacked.append(feature[0])
+            feature_intervals_unpacked.append(feature[1])
+        data_test_unpacked.append(feature_intervals_unpacked)
+
+    # Put all data in panda data frames with nicely named columns
+    data_test_df = pd.DataFrame(data_test_unpacked)
+    column_names_train = {}
+    column_names_test = {}
+    for feature in range(num_features):
+        column_names_train[feature] = 'x' + str(feature)
+        column_names_test[feature * 2] = 'x' + str(feature) + '_lower'
+        column_names_test[feature * 2 + 1] = 'x' + str(feature) + '_upper'
+    column_names_train[num_features] = 'y'
+    column_names_test[num_features * 2] = 'y_min'
+    column_names_test[num_features * 2 + 1] = 'y_max'
+    data_test_df.rename(columns=column_names_test, inplace=True)
+    return data_test_df
+
+
 # Samples uniformly from all intervals on [0,1)
 def interval_sample_uniform(num_features, ignore=None):
     feature_intervals = []
@@ -35,6 +204,15 @@ def interval_sample_uniform(num_features, ignore=None):
         )
 
     return feature_intervals
+
+def interval_sample_random_features(X):
+    feature_intervals = []
+    for feature in range(X.shape[1]):
+        feature_column = X[:, feature]
+        random_samples = np.random.choice(size=2, a=feature_column)
+        feature_intervals.append((np.min(random_samples), np.max(random_samples)))
+    return feature_intervals
+
 
 
 def interval_sample_length_exponential(numeric_min, numeric_max, categoric_features):
@@ -112,116 +290,6 @@ def interval_sample_random_points(numeric_features, categoric_features, X):
     return feature_intervals
 
 
-def stadardize_data(categoric_features, X, Y, numeric_features):
-    print("Standardizing...")
-    standardized_y = StandardScaler().fit_transform(Y)
-
-    # We don't want to standardize the categorical data
-    if len(categoric_features) is 0:
-        X_norm = StandardScaler().fit_transform(X)
-    else:
-        X_norm = CustomScaler(bin_vars_index=categoric_features, cont_vars_index=numeric_features).fit_transform(X)
-    return X_norm, np.ravel(standardized_y)
-
-
-def train_neural_network(X_gt_train, Y_gt_train):
-    print("Optimizing DNN...")
-
-    # MLP regressor with alpha-parameter and hidden layer sizes determined by grid search cross validation
-    param_grid = {'classify__alpha': 10.0 ** -np.arange(1, 7),
-                  'classify__activation': ['logistic'],
-                  'classify__hidden_layer_sizes': [(layer1, layer2)
-                                                   for layer1 in range(10, 28, 4)
-                                                   for layer2 in range(10, 28, 4)]}
-
-    synth_ground_truth_pipe = Pipeline([('classify', MLPRegressor())])
-    synth_ground_truth = GridSearchCV(synth_ground_truth_pipe, param_grid, n_jobs=1)
-    synth_ground_truth.fit(X_gt_train, Y_gt_train)
-    print("CV score:", synth_ground_truth.best_score_)
-    print("Finished optimizing, params are:")
-    print(synth_ground_truth.best_params_)
-    return synth_ground_truth
-
-
-def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_data_points, categoric_features,
-                     sampling_method):
-    # Generate interval-valued test data points for RQP
-    # by optimizing on random intervals on synthetic ground truth
-    data_test = []
-    num_features = X_RQP_train.shape[1]
-
-    numeric_min = []
-    numeric_max = []
-    for feature in numeric_features:
-        feature_column = X_RQP_train[:, feature]
-        numeric_min.append(np.min(feature_column))
-        numeric_max.append(np.max(feature_column))
-
-    for i in range(num_test_data_points):
-        NUM_RANDOM_RESTARTS = 10
-        if sampling_method == "length_uniform":
-            feature_intervals = interval_sample_length(num_features, categoric_features)
-        elif sampling_method == "uniform":
-            feature_intervals = interval_sample_uniform(numeric_features=numeric_features,
-                                                        categoric_features=categoric_features)
-        elif sampling_method == "random_points":
-            feature_intervals = interval_sample_random_points(numeric_features=numeric_features,
-                                                              categoric_features=categoric_features, X=X_RQP_train)
-        elif sampling_method == "length_exponential":
-            feature_intervals = interval_sample_length_exponential(numeric_min=numeric_min, numeric_max=numeric_max,
-                                                                   categoric_features=categoric_features)
-
-        # Restarts for blackbox optimization: middle of intervals plus 10 randomly chosen restarts
-        initial_guesses = []
-        mid_guess = []
-        for feature_interval in feature_intervals:
-            mid_guess.append(np.mean([feature_interval[0], feature_interval[1]]))
-        initial_guesses.append(mid_guess)
-        for j in range(NUM_RANDOM_RESTARTS):
-            rand_guess = []
-            for feature_interval in feature_intervals:
-                rand_feature_value = (feature_interval[1] - feature_interval[0]) \
-                                     * np.random.random_sample() + feature_interval[0]
-                rand_guess.append(rand_feature_value)
-            initial_guesses.append(rand_guess)
-        y_mins = []
-        y_maxs = []
-        for initial_guess in initial_guesses:
-            y_min_result = optimize.minimize(lambda x: synth_ground_truth.predict(x.reshape(1, -1)),
-                                             x0=initial_guess, bounds=feature_intervals)
-            y_max_result = optimize.minimize(lambda x: (-1) * synth_ground_truth.predict(x.reshape(1, -1)),
-                                             x0=initial_guess, bounds=feature_intervals)
-            y_mins.append(synth_ground_truth.predict(y_min_result.x.reshape(1, -1))[0])
-            y_maxs.append(synth_ground_truth.predict(y_max_result.x.reshape(1, -1))[0])
-        y_min = np.min(y_mins)
-        y_max = np.max(y_maxs)
-        feature_intervals.append((y_min, y_max))
-        data_test.append(feature_intervals)
-
-    # unpack intervals for test data
-    data_test_unpacked = []
-    for data in data_test:
-        feature_intervals_unpacked = []
-        for feature in data:
-            feature_intervals_unpacked.append(feature[0])
-            feature_intervals_unpacked.append(feature[1])
-        data_test_unpacked.append(feature_intervals_unpacked)
-
-    # Put all data in panda data frames with nicely named columns
-    data_test_df = pd.DataFrame(data_test_unpacked)
-    column_names_train = {}
-    column_names_test = {}
-    for feature in range(num_features):
-        column_names_train[feature] = 'x' + str(feature)
-        column_names_test[feature * 2] = 'x' + str(feature) + '_lower'
-        column_names_test[feature * 2 + 1] = 'x' + str(feature) + '_upper'
-    column_names_train[num_features] = 'y'
-    column_names_test[num_features * 2] = 'y_min'
-    column_names_test[num_features * 2 + 1] = 'y_max'
-    data_test_df.rename(columns=column_names_test, inplace=True)
-    return data_test_df
-
-
 def write_to_file(data_train_dfs, data_test_df, noises, sampling_method, dataset_name):
     stripped_dataset_name = dataset_name.replace('.arff', '')
     stripped_dataset_name = stripped_dataset_name.replace('regression/', '')
@@ -235,64 +303,10 @@ def write_to_file(data_train_dfs, data_test_df, noises, sampling_method, dataset
                         noises[i]) + "_RQPtrain.arff",
                     wekaname=file_path + "_RQP_training_data")
 
-# Creates the test-data & training data based on the given
-def create_test_data_internal(X, Y, N, numeric_features, dataset_name, categoric_features=[], num_test_data_points=None,
-                              sampling_method="random_points", noises=[0.1, 0.01, 0.001]):
-    if num_test_data_points is None:
-        num_test_data_points = N
-
-    X_gt_train, Y_gt_train = stadardize_data(categoric_features=categoric_features, X=X, Y=Y,
-                                             numeric_features=numeric_features)
-
-    synth_ground_truth = train_neural_network(X_gt_train=X_gt_train, Y_gt_train=Y_gt_train)
-
-    # Original X data with synthetic ground truth Y-values as training data for RQP
-    X_RQP_train = X_gt_train
-    Y_RQP_train = np.reshape(synth_ground_truth.predict(X_RQP_train), (N, 1))
-
-    column_names_train = {}
-    num_features = X_RQP_train.shape[1]
-
-    for feature in range(num_features):
-        column_names_train[feature] = 'x' + str(feature)
-
-    train_data = []
-    for noise in noises:
-        y_copy = np.copy(Y_RQP_train)
-        for i in range(len(y_copy)):
-            y_copy[i] = y_copy[i] + np.random.normal(0, noise)
-        data_train = np.concatenate([X_RQP_train, y_copy], axis=1)
-        data_train_df = pd.DataFrame(data_train)
-        data_train_df.rename(columns=column_names_train, inplace=True)
-        train_data.append(data_train_df)
-
-
-    data_test_df = create_rqp_data(X_RQP_train=X_RQP_train,
-                                                  numeric_features=numeric_features,
-                                                  categoric_features=categoric_features,
-                                                  synth_ground_truth=synth_ground_truth,
-                                                  num_test_data_points=num_test_data_points,
-                                                  sampling_method=sampling_method)
-
-    write_to_file(data_train_dfs=train_data, data_test_df=data_test_df, dataset_name=dataset_name, noises=noises,
-                      sampling_method=sampling_method)
-
-
-def create_test_data(file_path, sampling_method="uniform", num_test_data_points=None):
-    raw_data = pd.DataFrame(arff.loadarff(file_path)[0])
-    X = raw_data.iloc[:, :-1].get_values()
-    Y = raw_data.iloc[:, -1:]
-    N = raw_data.shape[0]
-    if num_test_data_points is None:
-        num_test_data_points = int(np.floor(0.3 * N))
-
-    return create_test_data_internal(N=N, X=X, Y=Y, dataset_name=file_path, sampling_method=sampling_method,
-                                     num_test_data_points=num_test_data_points, numeric_features=range(len(X[0])),
-                                     categoric_features=[])
 
 
 if __name__ == "__main__":
-    sampling_method = "length_exponential"
+    sampling_method = "random_features"
     file_paths = ["regression/boston.arff","regression/bank32nh.arff", "regression/bank8FM.arff", "regression/bodyfat.arff", "regression/cpu.small.arff", "regression/cal.housing.arff", "regression/elevators.arff", "regression/house8L.arff", "regression/kin8nm.arff", "regression/machine.cpu.arff"
                   ]
     for file_path in file_paths:
