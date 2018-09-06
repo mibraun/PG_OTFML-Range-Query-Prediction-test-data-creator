19c1baecfd3c9f0ee083dcbc0c8d0a2c8faed30c
Michael Johannes Braun <braunm@mail.upb.de>
2018-09-06 15:14:58 +0200

    - Various cleanups

--- RQP_test_data_creator.py
+++ RQP_test_data_creator.py
@@ -1,26 +1,24 @@
 import numpy as np
 import pandas as pd
 import os
-import util
-import random
-import pickle
 from pandas2arff import pandas2arff
-from CustomScaler import CustomScaler
 from sklearn.preprocessing import StandardScaler
 from scipy.io import arff
 from scipy import optimize
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-from random import randint
-
-### Constants:
-
-## The average depth of a tree in the heuristic search algorithm
-k = 8
 
 
-def create_test_data(file_path, sampling_method="uniform", num_test_data_points=None):
+def create_test_data(file_path, num_test_data_points=None):
+    """
+    Generates semi-synthetic data consisting of a set of precise training instances
+    and a set of interval-valued test instances based on a given data set.
+    Currently only supports numeric features.
+
+    :param file_path:  Path of the data set to base the semi-synthetic data on. Should be in .arff format.
+    :param num_test_data_points:  The number of interval-valued test instances to create. Default: 30% of training data size.
+    """
     raw_data = pd.DataFrame(arff.loadarff(file_path)[0])
     X = raw_data.iloc[:, :-1].get_values()
     Y = raw_data.iloc[:, -1:]
@@ -28,19 +26,43 @@ def create_test_data(file_path, sampling_method="uniform", num_test_data_points=
     if num_test_data_points is None:
         num_test_data_points = int(np.floor(0.3 * N))
 
-    return create_test_data_internal(N=N, X=X, Y=Y, dataset_name=file_path, sampling_method=sampling_method,
-                                     num_test_data_points=num_test_data_points, numeric_features=range(len(X[0])),
-                                     categoric_features=[])
+    create_test_data_internal(N=N, X=X, Y=Y, dataset_name=file_path, num_test_data_points=num_test_data_points,
+                              numeric_features=range(len(X[0])))
 
 
-# Creates the test-data & training data based on the given
-def create_test_data_internal(X, Y, N, numeric_features, dataset_name, categoric_features=[], num_test_data_points=None,
-                              sampling_method="random_points", noises=[0, 0.1, 0.3, 0.5]):
+def read_data_from_arff(train_file, test_file):
+    """Reads training and test data for range query prediction from arff files
+    and returns them as five numpy arrays.
+
+    Arguments:
+        train_file -- file path of the training data. Should be in .arff format.
+        test_file -- file path of the test data. Should be in .arff format.
+
+    Returns:
+        X_train -- 2-dimensional numpy array containing the training data features
+        Y_train -- 1-dimensional numpy array containing the training data target
+        X_test -- 2-dimensional numpy array containing the range query test data features in the form
+                    x1_min, x1_max, x2_min, x2_max, ...
+        Y_test_min -- 1-dimensional numpy array containing the test data target minima
+        Y_test_max -- 1-dimensional numpy array containing the test data target maxima
+    """
+
+    raw_data_train = pd.DataFrame(arff.loadarff(train_file)[0])
+    raw_data_test = pd.DataFrame(arff.loadarff(test_file)[0])
+    X_train = raw_data_train.iloc[:, :-1].get_values()
+    Y_train = raw_data_train.iloc[:, -1:]
+    X_test = raw_data_test.iloc[:, :-2].get_values()
+    Y_test_min = raw_data_test.iloc[:, -2:-1]
+    Y_test_max = raw_data_test.iloc[:, -1:]
+    return X_train, np.ravel(Y_train), X_test, np.ravel(Y_test_min), np.ravel(Y_test_max)
+
+
+# Creates the semi-synthetic test data & training data based on a given dataset
+def create_test_data_internal(X, Y, N, numeric_features, dataset_name, num_test_data_points=None, noises=[0, 0.1, 0.3, 0.5]):
     if num_test_data_points is None:
         num_test_data_points = N
 
-    X_gt_train, Y_gt_train = standardize_data(categoric_features=categoric_features, X=X, Y=Y,
-                                              numeric_features=numeric_features)
+    X_gt_train, Y_gt_train = standardize_data(X=X, Y=Y)
 
     synth_ground_truth = train_neural_network(X_gt_train=X_gt_train, Y_gt_train=Y_gt_train)
 
@@ -68,24 +90,16 @@ def create_test_data_internal(X, Y, N, numeric_features, dataset_name, categoric
 
     data_test_df = create_rqp_data(X_RQP_train=X_RQP_train,
                                    numeric_features=numeric_features,
-                                   categoric_features=categoric_features,
                                    synth_ground_truth=synth_ground_truth,
-                                   num_test_data_points=num_test_data_points,
-                                   sampling_method=sampling_method)
+                                   num_test_data_points=num_test_data_points)
 
-    write_to_file(data_train_dfs=train_data, data_test_df=data_test_df, dataset_name=dataset_name, noises=noises,
-                  sampling_method=sampling_method)
+    write_to_file(data_train_dfs=train_data, data_test_df=data_test_df, noises=noises, dataset_name=dataset_name)
 
 
-def standardize_data(categoric_features, X, Y, numeric_features):
+def standardize_data(X, Y):
     print("Standardizing...")
     standardized_y = StandardScaler().fit_transform(Y)
-
-    # We don't want to standardize the categorical data
-    if len(categoric_features) is 0:
-        X_norm = StandardScaler().fit_transform(X)
-    else:
-        X_norm = CustomScaler(bin_vars_index=categoric_features, cont_vars_index=numeric_features).fit_transform(X)
+    X_norm = StandardScaler().fit_transform(X)
     return X_norm, np.ravel(standardized_y)
 
 
@@ -108,8 +122,7 @@ def train_neural_network(X_gt_train, Y_gt_train):
     return synth_ground_truth
 
 
-def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_data_points, categoric_features,
-                    sampling_method):
+def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_data_points):
     # Generate interval-valued test data points for RQP
     # by optimizing on random intervals on synthetic ground truth
     data_test = []
@@ -124,19 +137,7 @@ def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_
 
     for i in range(num_test_data_points):
         NUM_RANDOM_RESTARTS = 10
-        if sampling_method == "length_uniform":
-            feature_intervals = interval_sample_length(num_features, categoric_features)
-        elif sampling_method == "uniform":
-            feature_intervals = interval_sample_uniform(numeric_features=numeric_features,
-                                                        categoric_features=categoric_features)
-        elif sampling_method == "random_points":
-            feature_intervals = interval_sample_random_points(numeric_features=numeric_features,
-                                                              categoric_features=categoric_features, X=X_RQP_train)
-        elif sampling_method == "length_exponential":
-            feature_intervals = interval_sample_length_exponential(numeric_min=numeric_min, numeric_max=numeric_max,
-                                                                   categoric_features=categoric_features)
-        elif sampling_method == "random_features":
-            feature_intervals = interval_sample_random_features(X_RQP_train)
+        feature_intervals = interval_sample_random_features(X_RQP_train)
 
         # Restarts for blackbox optimization: middle of intervals plus 10 randomly chosen restarts
         initial_guesses = []
@@ -186,23 +187,7 @@ def create_rqp_data(X_RQP_train, synth_ground_truth, numeric_features, num_test_
     return data_test_df
 
 
-# Samples uniformly from all intervals on [0,1)
-def interval_sample_uniform(num_features, ignore=None):
-    feature_intervals = []
-    for feature in range(num_features):
-        if feature in ignore:
-            continue
-        x1 = np.random.random_sample()
-        x2 = np.random.random_sample()
-        lower_bound = np.min([x1, x2])
-        upper_bound = np.max([x1, x2])
-        feature_intervals.append(
-            (lower_bound, upper_bound)
-        )
-
-    return feature_intervals
-
-
+# Samples intervals by choosing two values x_i and x'_i for each feature i from the data and using them as interval endpoints.
 def interval_sample_random_features(X):
     feature_intervals = []
     for feature in range(X.shape[1]):
@@ -213,82 +198,7 @@ def interval_sample_random_features(X):
     return feature_intervals
 
 
-def interval_sample_length_exponential(numeric_min, numeric_max, categoric_features):
-    feature_intervals = []
-    ## categoric features
-    # we assume one hot encoding
-    if len(categoric_features) is not 0:
-        categoric_max_interval = len(categoric_features)
-        print("Categoric max interval length: " + str(categoric_max_interval))
-        categoric_expected_interval_length = (float(k) * categoric_max_interval) / ((2 ** k) - 1)
-        print("Categoric expected interval length: " + str(categoric_expected_interval_length))
-        categoric_random_interval_length = np.random.exponential(scale=0.5)
-        print("Categoric actual interval length: " + str(categoric_random_interval_length))
-        indices = random.sample(range(0, len(categoric_features)), int(np.ceil(categoric_random_interval_length)))
-
-        for i in range(0, len(categoric_features)):
-            if i in indices:
-                feature_intervals.append((1, 1))
-            else:
-                feature_intervals.append((0, 0))
-
-    print("Numeric max interval length: " + str(numeric_max))
-    ## numeric features
-    for i in range(0, len(numeric_min)):
-        numeric_length = numeric_max[i] - numeric_min[i]
-        numeric_expected_interval_length = (float(k) * numeric_length) / ((2 ** k) - 1)
-        numeric_random_length = np.random.exponential(scale=numeric_expected_interval_length)
-        max_lower_bound = numeric_max[i] - numeric_random_length
-        lower_bound = np.random.uniform(low=numeric_min[i], high=max_lower_bound)
-        feature_intervals.append((lower_bound, lower_bound + numeric_random_length))
-        print("Numeric expected interval length: " + str(numeric_expected_interval_length))
-        print("Numeric actual interval length: " + str(numeric_random_length))
-    return feature_intervals
-
-
-# Samples intervals on [0,1), such that all interval lengths are represented uniformly
-def interval_sample_length(numeric_features, categoric_features):
-    feature_intervals = []
-
-    for feature in range(0, len(numeric_features)):
-        interval_length = np.random.random_sample()
-        lower_bound = interval_length * np.random.random_sample()
-        upper_bound = lower_bound + interval_length
-        feature_intervals.append(
-            (lower_bound, upper_bound)
-        )
-
-    for i in range(0, len(categoric_features)):
-        value = randint(0, 1)
-        feature_intervals.append((value, value))
-    return feature_intervals
-
-
-def interval_sample_random_points(numeric_features, categoric_features, X):
-    feature_intervals = []
-    randindex1 = randint(0, len(X) - 1)
-    randindex2 = randint(0, len(X) - 1)
-    random_point1 = X[randindex1]
-    random_point2 = X[randindex2]
-
-    for feature in numeric_features:
-        lower_bound = min(random_point1[feature], random_point2[feature])
-        upper_bound = max(random_point1[feature], random_point2[feature])
-        feature_intervals.append(
-            (lower_bound, upper_bound)
-        )
-
-    for feature in categoric_features:
-        lower_bound = max(random_point1[feature], random_point2[feature])
-        upper_bound = max(random_point1[feature], random_point2[feature])
-        feature_intervals.append(
-            (lower_bound, upper_bound)
-        )
-
-    return feature_intervals
-
-
-def write_to_file(data_train_dfs, data_test_df, noises, sampling_method, dataset_name):
+def write_to_file(data_train_dfs, data_test_df, noises, dataset_name, sampling_method="random_features"):
     stripped_dataset_name = dataset_name.replace('.arff', '')
     stripped_dataset_name = stripped_dataset_name.replace('regression/', '')
     pandas2arff(data_test_df,
@@ -302,33 +212,6 @@ def write_to_file(data_train_dfs, data_test_df, noises, sampling_method, dataset
                     wekaname=file_path + "_RQP_training_data")
 
 
-def read_data_from_arff(train_file, test_file):
-    """Reads training and test data for range query prediction from arff files
-    and returns them as five numpy arrays.
-
-    Arguments:
-        train_file -- file path of the training data. Should be in .arff format.
-        test_file -- file path of the test data. Should be in .arff format.
-
-    Returns:
-        X_train -- 2-dimensional numpy array containing the training data features
-        Y_train -- 1-dimensional numpy array containing the training data target
-        X_test -- 2-dimensional numpy array containing the range query test data features in the form
-                    x1_min, x1_max, x2_min, x2_max, ...
-        Y_test_min -- 1-dimensional numpy array containing the test data target minima
-        Y_test_max -- 1-dimensional numpy array containing the test data target maxima
-    """
-
-    raw_data_train = pd.DataFrame(arff.loadarff(train_file)[0])
-    raw_data_test = pd.DataFrame(arff.loadarff(test_file)[0])
-    X_train = raw_data_train.iloc[:, :-1].get_values()
-    Y_train = raw_data_train.iloc[:, -1:]
-    X_test = raw_data_test.iloc[:, :-2].get_values()
-    Y_test_min = raw_data_test.iloc[:, -2:-1]
-    Y_test_max = raw_data_test.iloc[:, -1:]
-    return X_train, np.ravel(Y_train), X_test, np.ravel(Y_test_min), np.ravel(Y_test_max)
-
-
 if __name__ == "__main__":
     sampling_method = "random_features"
     file_paths = ["regression/boston.arff", "regression/bank32nh.arff", "regression/bank8FM.arff",
@@ -339,7 +222,7 @@ if __name__ == "__main__":
     for file_path in file_paths:
         try:
             os.makedirs("data/" + file_path + "/" + sampling_method, exist_ok=True)
-            create_test_data(file_path, sampling_method=sampling_method)
+            create_test_data(file_path)
         except RuntimeError as err:
             print(err)
             pass
